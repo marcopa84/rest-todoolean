6c5251d15a1744089ca0b5d827f5f08f02bb8af3
Marco <marcopa84@hotmail.it>
2020-02-13 16:36:59 +0100

    get request OK

--- js/script.js
+++ js/script.js
@@ -1,23 +1,33 @@
 $(document).ready(function() {
-  
+
+  getList();
 
 
 });
 
+function getList() {
+  var source = $('#todo-template').html();
+  var template = Handlebars.compile(source);
+  $.ajax(
+    {
+      url: "http://157.230.17.132:3028/todos",
+      method: "GET",
+      success: function (data, stato) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+          console.log(data[i].id);
+          console.log(data[i].text);
+          var context = {
+            id: data[i].id,
+            text: data[i].text
+          }
+          var html = template(context);
+          $('.todo_list').append(html);
+        }
 
-
-
-
-
-
-// $.ajax(
-//   {
-//     url: " URL DA INSERIRE",
-//     method: "GET",
-//     success: function (data, stato) {
-//       console.log(data.response);
-//     },
-//     error: function (richiesta, stato, errori) {
-//       alert("E' avvenuto un errore. " + errore);
-//     }
-// });
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+  });
+}
