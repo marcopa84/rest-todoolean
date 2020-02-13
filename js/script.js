a8da098ac263310c54b27c1d47a638d07ad8f348
Marco <marcopa84@hotmail.it>
2020-02-13 16:07:40 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  
+
+
+});
+
+
+
+
+
+
+
+// $.ajax(
+//   {
+//     url: " URL DA INSERIRE",
+//     method: "GET",
+//     success: function (data, stato) {
+//       console.log(data.response);
+//     },
+//     error: function (richiesta, stato, errori) {
+//       alert("E' avvenuto un errore. " + errore);
+//     }
+// });
