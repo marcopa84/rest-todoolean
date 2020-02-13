ec49faf3556c4693a7c66a0101fe3ac96215a9bf
Marco <marcopa84@hotmail.it>
2020-02-13 17:01:18 +0100

    delete OK add OK

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
 
   getList();
 
+  $(document).on('click', '.todo_command', function () {
+    var thisElementId = $(this).parents('li').attr('data_id')
+    console.log(thisElementId);
+    deleteIthem(thisElementId);
+  });
+
+  $(document).on('click', '#add_button', function () {
+    var addValue = $('#add_input').val();
+    addIthem(addValue)
+  });
+
 
 });
 
@@ -15,8 +26,6 @@ function getList() {
       success: function (data, stato) {
         console.log(data);
         for (var i = 0; i < data.length; i++) {
-          console.log(data[i].id);
-          console.log(data[i].text);
           var context = {
             id: data[i].id,
             text: data[i].text
@@ -24,6 +33,41 @@ function getList() {
           var html = template(context);
           $('.todo_list').append(html);
         }
+
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+  });
+}
+function deleteIthem(id) {
+  $.ajax(
+    {
+      url: "http://157.230.17.132:3028/todos/"+id,
+      method: "DELETE",
+      success: function (data, stato) {
+        $('.todo_list').html('');
+        getList();
+
+      },
+      error: function (richiesta, stato, errori) {
+        alert("E' avvenuto un errore. " + errore);
+      }
+  });
+}
+
+function addIthem(data) {
+  $.ajax(
+    {
+      url: "http://157.230.17.132:3028/todos",
+      method: "POST",
+      data:{
+        text: data,
+      },
+      success: function (data, stato) {
+        $('#add_input').val('');
+        $('.todo_list').html('');
+        getList();
 
       },
       error: function (richiesta, stato, errori) {
